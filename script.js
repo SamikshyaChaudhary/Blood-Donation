22b32f7bb4a273adc9dec9c030e159153a016dbc
Samikshya <samikshyachaudhary15@gmail.com>
2026-05-22 20:50:25 +0545

    Initialze repo with some html files

---
+++ script.js
@@ -0,0 +1,18 @@
+// Donate Button
+
+document.querySelector(".donate-btn")
+.addEventListener("click", function(){
+
+  alert("Thank you for donating blood ❤️");
+
+});
+
+
+// Find Blood Button
+
+document.querySelector(".find-btn")
+.addEventListener("click", function(){
+
+  alert("Searching blood donors...");
+
+});
